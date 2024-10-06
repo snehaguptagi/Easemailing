1ecd1e78cc10b2d3523b7ddfdc802dac5c005811
SNEHA GUPTA <166572468+SNEHA-81@users.noreply.github.com>
2024-10-06 21:24:17 +0530

    Update and rename import os.py to extract_email_info.py

--- extract_email_info.py
+++ extract_email_info.py
@@ -1,11 +1,14 @@
 import re
 from dateutil.parser import parse
+from collections import namedtuple
 
-def extract_email_details(email_body):
-    details = {}
+EmailDetails = namedtuple('EmailDetails', ['dates', 'times', 'contacts', 'attachments'])
+
+def extract_email_details(email_body: str) -> EmailDetails:
+    details = {'dates': [], 'times': [], 'contacts': [], 'attachments': []}
 
     # Extract dates
-    date_pattern = r'\b(\d{1,2} [A-Za-z]+ \d{4}|\d{4}-\d{2}-\d{2})\b'
+    date_pattern = r'\b(\d{1,2} [A-Za-z]{3,9} \d{4}|\d{4}-\d{2}-\d{2}|\d{1,2}/\d{1,2}/\d{2,4})\b'
     dates = re.findall(date_pattern, email_body)
     if dates:
         details['dates'] = [parse(date).date() for date in dates]
@@ -17,18 +20,17 @@ def extract_email_details(email_body):
         details['times'] = times
 
     # Extract contact names
-    contact_pattern = r'\b[A-Z][a-z]+ [A-Z][a-z]+\b'
+    contact_pattern = r'\b[A-Z][a-z]+(?: [A-Z][a-z]+)*\b'
     contacts = re.findall(contact_pattern, email_body)
     if contacts:
         details['contacts'] = contacts
-
     # Extract attachments (assuming attachments are mentioned in the email body)
-    attachment_pattern = r'\b\w+\.\w+\b'
+    attachment_pattern = r'\b\w+\.\w{2,4}\b'
     attachments = re.findall(attachment_pattern, email_body)
     if attachments:
         details['attachments'] = attachments
 
-    return details
+    return EmailDetails(**details)
 
 # Example usage
 email_body = """
